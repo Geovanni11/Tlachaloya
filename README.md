e0feaf5694ea07e05d750767de5da540d3398481
Geovanni11 <alan.geo10@gmail.com>
2019-04-28 18:34:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tlachaloya.github.com
---
+++ data/GEOFT_1.js
@@ -0,0 +1,26 @@
+var json_GEOFT_1 = {
+"type": "FeatureCollection",
+"name": "GEOFT_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/6i5q67sa3jtmino\/IMG-2838.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.682852777777782, 19.413358333333331 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/1qc6pmaxx6wasll\/IMG-2845.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.680527777777783, 19.414202777777774 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/bifdau4dztjcllj\/IMG-2856.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.665794444444444, 19.434172222222223 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/yuuyi0upb7xng3l\/IMG-2857.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.659683333333348, 19.4432 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/50phkhx6xx4agv0\/IMG-2860.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.659797222222224, 19.4438 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/611u0kw3xvx27pw\/IMG-2864.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.660330555555575, 19.445544444444444 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/6x25ss22q5zxeb1\/IMG-2875.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.663461111111118, 19.448244444444445 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/u2k537ijdlozarn\/IMG-2886.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.664261111111117, 19.450005555555553 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/ov8la1npdn328v1\/IMG_20190417_160032.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.662231445277783, 19.446229934444442 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/od2p3t9mcyj49q9\/IMG_20190417_162541.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.660896301111123, 19.446187972777775 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/oclah0fpzfj4hi5\/IMG_20190417_162844.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.662345886111112, 19.447525024166662 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/oftqom0me2c27l7\/IMG_20190417_163433.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.664299011111112, 19.449724197222221 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/70y9gc7dsnxcj27\/IMG_20190417_163457.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.66431427, 19.449804305833332 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/438jxedik2ghejx\/IMG_20190417_163510.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.66431427, 19.449804305833332 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/7pbdd01of7lyl86\/IMG_20190417_163558.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.664337158055559, 19.45002746555555 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/4nikrm0wogob2bu\/IMG_20190417_163622.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.664337158055559, 19.45002746555555 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/x4qy28r5xvofu5y\/IMG_20190417_164135.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.65901947, 19.450469970555552 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/adbbyapllgph0ly\/IMG_20190417_165146.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.66352844222223, 19.447086334166666 ] } },
+{ "type": "Feature", "properties": { "html_exp": "<img src=https:\/\/dl.dropboxusercontent.com\/s\/1pmlh0f51z4cbh5\/IMG_20190417_170701.jpg>" }, "geometry": { "type": "Point", "coordinates": [ -99.661170959444448, 19.445737838611109 ] } }
+]
+}
